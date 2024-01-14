ac679e36f4f1e8f24c7b9c129aaee74ddef4e2c3
Suray Marag Pandit <suraymaragpandit@gmail.com>
2024-01-14 22:29:46 +0530

    practise

--- BinaryTree/Lecture62/06_L62_BuildFromLevelOrder.cpp
+++ BinaryTree/Lecture62/06_L62_BuildFromLevelOrder.cpp
@@ -8,20 +8,88 @@ class node {
     int data;
     node * left;
     node * right;
+
+    node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+
+    }
 };
 
-node * buildFromLevelOrder(node * root){
+void buildFromLevelOrder(node * &root){
     queue<node * > q;
+
+    cout<<"Enter data for root: ";
+    int rootdata;
+    cin>>rootdata;
+
+    root = new node(rootdata);
 
     q.push(root);
 
-    while(q.empty()){
+    while(!q.empty()){
+        node * temp = q.front();
+        q.pop();
+
+        cout<<"Enter data for left "<<temp->data<<": ";
+        int ldata;
+        cin>>ldata;
+
+        if(ldata!=-1){
+            temp->left = new node(ldata);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter data in right "<<temp->data<<" : ";
+        int rdata;
+        cin>>rdata;
+        
+        if(rdata!=-1){
+            temp->right = new node(rdata);
+            q.push(temp->right);
+        }
     
     }
-
 }
+
+
+void bfs(node * root){
+    queue<node * > q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node * temp = q.front();
+        q.pop();
+
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+
+            if(temp->left)
+                q.push(temp->left);
+            if(temp->right)
+                q.push(temp->right);
+        }
+    }
+    
+}
+
 int main(){
 
-    
+    node * root = NULL;
+
+    buildFromLevelOrder(root);
+
+    bfs(root);
+
+
     return 0;
 }
