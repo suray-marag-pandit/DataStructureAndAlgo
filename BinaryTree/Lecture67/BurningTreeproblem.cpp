73ef94b3c945b6f235b0733798e39757311ff97a
Suray <suraymaragpandit@gmail.com>
2024-02-10 17:08:11 +0530

    BurningTreeproblem

--- BinaryTree/Lecture67/BurningTreeproblem.cpp
+++ BinaryTree/Lecture67/BurningTreeproblem.cpp
@@ -1,46 +1,142 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
+#include <map>
 using namespace std;
 
-class node{
-    public:
-    int data;
-    node * left;
-    node * right;
+// creating node Structure
+class node
+{
 
-    node(int data){
-        this->data  = data;
-        this->left  =   NULL;
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
         this->right = NULL;
     }
 };
 
-node * build(node * root){
-    cout<<"Enter the data";
+node *build(node *root)
+{
+    // cout << "Enter the data: ";
     int data;
-    cin>>data;
+    cin >> data;
 
-    if(data==-1){
+    // to create leaf node children null
+    if (data == -1)
         return NULL;
-    }
 
     root = new node(data);
 
-    cout<<"Enter the data for the left: ";
+    // to insert data in the left of the current postion
+    // cout << "To the left of " << data << endl;
     root->left = build(root->left);
 
-    cout<<"Enter the data for the right: ";
+    // to insert data in the right of the current postion
+    // cout << "To the right of " << data << endl;
     root->right = build(root->right);
 
+    // to place node
     return root;
 }
 
-
-int main(int argc, char const *argv[])
+node *createmap(node *root, int target, map<node *, node *> &m)
 {
-    //1 2 3 7 -1 -1 -1 4 -1 -1 -1 5 -1 6 -1 8 -1 -1
 
-    node * root = NULL;
+    queue<node *> q;
+    q.push(root);
+
+    node *targetnode=NULL;
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop(); 
+
+        if (temp->left)
+        {
+            m[temp->left] = temp;
+            q.push(temp->left);
+        }
+
+        if (temp->right)
+        {
+            m[temp->right] = temp;
+            q.push(temp->right);
+        }
+
+        if (temp->data == target)
+        {
+            targetnode = temp;
+        }
+    }
+    return targetnode;
+}
+
+int burn(node *target, map<node *, node* > &m)
+{
+    map<node *, bool> visited;
+    queue<node *> travel;
+    int time = 0;
+
+    travel.push(target);
+    visited[target] = true;
+
+    while (!travel.empty())
+    {
+
+        node *temp = travel.front();
+        travel.pop();
+
+        bool flag= false;
+
+        if (temp->left && visited[temp->left] == false)
+        {
+            visited[temp->left] = true;
+            travel.push(temp->left);
+            flag=true;
+        }
+
+        if (temp->right && visited[temp->right] == false)
+        {
+            visited[temp->right] = true;
+            travel.push(temp->right);
+            flag=true;
+        }
+
+        if (m[temp] && visited[m[temp]] == false)
+        {
+            visited[m[temp]] = true;
+            travel.push(m[temp]);
+            flag=true;
+        }
+
+        if (flag)
+        {
+            time++;
+        }
+    }
+
+    return time;
+}
+
+int main()
+{
+    node *root = NULL;
+
     root = build(root);
+    map<node*,node*> m;
 
+    node * target = createmap(root,8,m);
+
+    int time = burn(target,m);
+    // 10 20 30 40 -1 -1 50 -1 -1 -1 60 70 -1 -1 80 -1 -1
+
+    //1 2 4 -1 -1 5 7 -1 -1 8 -1 -1 3 -1 6 -1 9 -1 10 -1 -1
+    cout<<time<<endl;
     return 0;
 }
