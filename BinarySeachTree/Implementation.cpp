be900fc7e789b1cad596b4745476bdfce74ce141
Suray <suraymaragpandit@gmail.com>
2024-02-17 21:25:56 +0530

    Implementation

---
+++ BinarySeachTree/Implementation.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *insert(node * &root, int data)
+{
+
+    if (root == NULL)
+    {
+        node *temp = new node(data);
+        return temp;
+    }
+
+    if (data >= root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    else
+    {
+        root->left = insert(root->left, data);
+    }
+
+    return root;
+}
+
+void takeinput(node * &root)
+{
+
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        insert(root, data);
+        cin >> data;
+    }
+}
+
+//to display the level order in one line 
+void LinearOutputBFS(node * root){
+
+    //queue creation
+    queue<node *> q;
+
+    q.push(root);
+
+    cout<<endl<<"The linear output for level order traversal: "<<endl;
+    while(!q.empty()){
+
+        node * temp = q.front();
+        q.pop();
+
+        cout<<temp->data<<" ";
+
+        //taking left child
+        if(temp->left)
+            q.push(temp->left);
+
+        //taking right child
+        if(temp->right)
+            q.push(temp->right);
+
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    node *root = NULL;
+
+    // 10 8 21 7 27 5 4 3 -1
+    cout<<"Enter the data";
+    takeinput(root);
+    LinearOutputBFS(root);
+    return 0;
+}
