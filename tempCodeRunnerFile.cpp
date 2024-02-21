9a2c22a49a9e36a71b4f8c7aa1a185e759f02e9c
Suray <suraymaragpandit@gmail.com>
2024-02-21 19:21:35 +0530

    deletionInBST

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,149 +1,78 @@
-// C++ program to implement optimized delete in BST.
+// C++ program to Calculate Waiting
+// Time for given Processes
 #include <iostream>
 using namespace std;
 
-struct Node {
-	int key;
-	struct Node *left, *right;
-};
-
-// A utility function to create a new BST node
-Node* newNode(int item)
+// Function to Calculate waiting time
+// and average waiting time
+void CalculateWaitingTime(int at[],
+						int bt[], int N)
 {
-	Node* temp = new Node;
-	temp->key = item;
-	temp->left = temp->right = NULL;
-	return temp;
+
+	// Declare the array for waiting
+	// time
+	int wt[N];
+
+	// Waiting time for first process
+	// is 0
+	wt[0] = 0;
+
+	// Print waiting time process 1
+	cout << "PN\t\tAT\t\t"
+		<< "BT\t\tWT\n\n";
+	cout << "1"
+		<< "\t\t" << at[0] << "\t\t"
+		<< bt[0] << "\t\t" << wt[0] << endl;
+
+	// Calculating waiting time for
+	// each process from the given
+	// formula
+	for (int i = 1; i < 5; i++) {
+		wt[i] = (at[i - 1] + bt[i - 1]
+				+ wt[i - 1]) - at[i];
+
+		// Print the waiting time for
+		// each process
+		cout << i + 1 << "\t\t" << at[i]
+			<< "\t\t" << bt[i] << "\t\t"
+			<< wt[i] << endl;
+	}
+
+	// Declare variable to calculate
+	// average
+	float average;
+	float sum = 0;
+
+	// Loop to calculate sum of all
+	// waiting time
+	for (int i = 0; i < 5; i++) {
+		sum = sum + wt[i];
+	}
+
+	// Find average waiting time
+	// by dividing it by no. of process
+	average = sum / 5;
+
+	// Print Average Waiting Time
+	cout << "\nAverage waiting time = "
+		<< average;
 }
 
-// A utility function to do inorder traversal of BST
-void inorder(Node* root)
-{
-	if (root != NULL) {
-		inorder(root->left);
-		printf("%d ", root->key);
-		inorder(root->right);
-	}
-}
-
-/* A utility function to insert a new node with given key in
-* BST */
-Node* insert(Node* node, int key)
-{
-	/* If the tree is empty, return a new node */
-	if (node == NULL)
-		return newNode(key);
-
-	/* Otherwise, recur down the tree */
-	if (key < node->key)
-		node->left = insert(node->left, key);
-	else
-		node->right = insert(node->right, key);
-
-	/* return the (unchanged) node pointer */
-	return node;
-}
-
-/* Given a binary search tree and a key, this function
-deletes the key and returns the new root */
-Node* deleteNode(Node* root, int k)
-{
-	// Base case
-	if (root == NULL)
-		return root;
-
-	// Recursive calls for ancestors of
-	// node to be deleted
-	if (root->key > k) {
-		root->left = deleteNode(root->left, k);
-		return root;
-	}
-	else if (root->key < k) {
-		root->right = deleteNode(root->right, k);
-		return root;
-	}
-
-	// We reach here when root is the node
-	// to be deleted.
-
-	// If one of the children is empty
-	if (root->left == NULL) {
-		Node* temp = root->right;
-		delete root;
-		return temp;
-	}
-	else if (root->right == NULL) {
-		Node* temp = root->left;
-		delete root;
-		return temp;
-	}
-
-	// If both children exist
-	else {
-
-		Node* succParent = root;
-
-		// Find successor
-		Node* succ = root->right;
-		while (succ->left != NULL) {
-			succParent = succ;
-			succ = succ->left;
-		}
-
-		// Delete successor. Since successor
-		// is always left child of its parent
-		// we can safely make successor's right
-		// right child as left of its parent.
-		// If there is no succ, then assign
-		// succ->right to succParent->right
-		if (succParent != root)
-			succParent->left = succ->right;
-		else
-			succParent->right = succ->right;
-
-		// Copy Successor Data to root
-		root->key = succ->key;
-
-		// Delete Successor and return root
-		delete succ;
-		return root;
-	}
-}
-
-// Driver Code
+// Driver code
 int main()
 {
-	/* Let us create following BST
-			50
-		/	 \
-		30	 70
-		/ \ / \
-	20 40 60 80 */
-	Node* root = NULL;
-	root = insert(root, 50);
-	root = insert(root, 30);
-	root = insert(root, 20);
-	root = insert(root, 40);
-	root = insert(root, 70);
-	root = insert(root, 60);
+	// Number of process
+	int N = 5;
 
-	printf("Original BST: ");
-	inorder(root);
+	// Array for Arrival time
+	int at[] = { 0, 1, 2, 3, 4 };
 
-	printf("\n\nDelete a Leaf Node: 20\n");
-	root = deleteNode(root, 20);
-	printf("Modified BST tree after deleting Leaf Node:\n");
-	inorder(root);
+	// Array for Burst Time
+	int bt[] = { 4, 3, 1, 2, 5 };
 
-	printf("\n\nDelete Node with single child: 70\n");
-	root = deleteNode(root, 70);
-	printf("Modified BST tree after deleting single child Node:\n");
-	inorder(root);
-
-	printf("\n\nDelete Node with both child: 50\n");
-	root = deleteNode(root, 50);
-	printf("Modified BST tree after deleting both child Node:\n");
-	inorder(root);
-
+	// Function call to find
+	// waiting time
+	CalculateWaitingTime(at, bt, N);
 	return 0;
 }
+//this code is contributed by snehalsalokhe
