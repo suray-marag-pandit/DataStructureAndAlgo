f15ed2b57c6dddeffcd4836276fc031225bb34a8
Suray <suraymaragpandit@gmail.com>
2024-02-27 19:57:39 +0530

    heapimplementation

---
+++ Practice/heapimplementation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    int arr[100];
+    int size;
+
+    public:
+    void insertion(int value){
+        size = size+1;
+        
+
+    }
+
+
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
