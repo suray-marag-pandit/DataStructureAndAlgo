4f44a518656c4be53d362c3ee483aa6dbdca64e9
Suray <suraymaragpandit@gmail.com>
2024-03-13 00:13:24 +0530

    DijkstraAlgorithm

--- graphs/DijkstraAlgorithm.cpp
+++ graphs/DijkstraAlgorithm.cpp
@@ -8,15 +8,36 @@ using namespace std;
 
 
 class adj{
+    public:
     int a,b,w;
-    adj(int a,int b,int w);
+    adj(int a,int b,int w){
+        this->a=a;
+        this->b=b;
+        this->w=w;
+    }
 };
+void adjL(int a,int b,int w,vector<adj> &v){
+    v.push_back(adj(a,b,w));
+    v.push_back(adj(b,a,w));
 
+}
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    vector<adj> v;
+    adjL(0,1,5,v);
+    adjL(0,2,8,v);
+    adjL(1,2,9,v);
+    adjL(1,3,2,v);
+    adjL(3,2,6,v);
+    adjL(0,2,8,v);
+
+    for (auto i : v)
+    {
+        cout<<i.a<<"("<<i.b<<","<<i.w<<")"<<endl;
+    }
+    
     return 0;
 }
 
