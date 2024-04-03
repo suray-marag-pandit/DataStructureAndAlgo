3ff59f1020d54e19c0d09b0be7b9151228c131e9
Suray <suraymaragpandit@gmail.com>
2024-04-03 10:57:23 +0530

    gfg

---
+++ gfg/bstkthancestor.cpp
@@ -0,0 +1,27 @@
+int kthCommonAncestor(Node *root, int k,int x, int y)
+{
+    vector<int> arr;
+    Node* t = root;
+    int mn = min(x,y);
+    int mx = max(x,y);
+    while(t!=NULL)
+    {
+        if(t->data < mn) 
+        {
+            arr.push_back(t->data);
+            t=t->right;
+        }
+        else if(t->data > mx) 
+        {
+            arr.push_back(t->data);
+            t=t->left;
+        }
+        else
+        {
+            arr.push_back(t->data);
+            if(k>arr.size()) return -1;
+            return arr[arr.size()-k];
+        }
+    }
+    return -1;
+}
